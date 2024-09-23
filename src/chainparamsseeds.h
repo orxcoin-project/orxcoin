b2e32b87d61619357141871f8a1a232924f3f131
wrathmdx <sjsidhu93@gmail.com>
2024-09-23 07:41:12 +0530

    Update chainparamsseeds.h

--- src/chainparamsseeds.h
+++ src/chainparamsseeds.h
@@ -7,9 +7,10 @@
  * Each line contains a BIP155 serialized (networkID, addr, port) tuple.
  */
 static const uint8_t chainparams_seed_main[] = {
+    0x01,0x04,0x03,0x44,0x22,0xc4,0x24,0x75,
 };
 
 static const uint8_t chainparams_seed_test[] = {
-    
+     0x01,0x04,0xd9,0x48,0x04,0x9d,0x4b,0x87,
 };
 #endif // BITCOIN_CHAINPARAMSSEEDS_H
